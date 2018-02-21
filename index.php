a3ebf8fae274d130734a8c6d1c20efc64538c828
Satel Importadores <satelimportadores@gmail.com>
2018-02-21 11:11:50 -0500

    Creacion de ajax y funcion

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Conjetura Romero</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+	<h1>Conjetura Romero</h1>
+		<h2>Todo numero primo mayor que 37 elevado a la pontencia de 10 es PAR</h2>
+		<div class="imprimir" id="imprimir">
+			
+		</div>
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/index.js"></script>
+</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	$.ajax({
+		url: 'json/primos.php',
+		type: 'POST'
+	})
+	.done(function(data) {
+			//console.log("success");
+			//$('#imprimir').html(data);
+		Arrayprimos = jQuery.parseJSON(data);
+		C_ROMERO(Arrayprimos);
+	})
+	
+});
+
+C_ROMERO = function(Arrayprimos){
+
+
+		for (var i = 0; i <= 30; i++) {
+			numerooriginal = Arrayprimos[i];
+			numero = numerooriginal^10;
+				if (numero % 2 == 0) {
+							log = $('#imprimir').html();
+							$('#imprimir').html(log+numerooriginal+'-^- '+numero+' ES PAR '+'<br>');
+
+				}else{
+							log = $('#imprimir').html();
+							$('#imprimir').html(log+numerooriginal+'-^-'+numero+' ES IMPAR '+'<br>');
+				}
+
+		};
+	
+            
+}
